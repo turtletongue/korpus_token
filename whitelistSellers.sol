e0cee681f044761f0451ad6563d80366f594ba84
PlatonSterh <platon.s14@list.ru>
2019-10-23 18:06:20 +0500

    Add import Ownable contract

--- whitelistSellers.sol
+++ whitelistSellers.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.24;
 
+import "https://github.com/PlatonSterh/korpus_token/Ownable.sol";
+
 contract whitelistSellers is Ownable {
     
     mapping(address => bool) public sellers;
