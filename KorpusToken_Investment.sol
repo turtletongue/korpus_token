460d7fbfa08272353bf3462d934aaf3eddb6dda4
PlatonSterh <platon.s14@list.ru>
2019-10-23 19:48:29 +0500

    Beauty changes

--- KorpusToken_Investment.sol
+++ KorpusToken_Investment.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.1;
 import "https://github.com/PlatonSterh/korpus_token/Ownable.sol";
 import "https://github.com/PlatonSterh/korpus_token/SafeMath.sol";
 
-contract KorpusToken_Investment is Ownable {
+contract KorpusToken_Deposit is Ownable {
     
     // Переменная с числом выпущенных и существующих токенов.
     uint256 public totalSupply;
@@ -22,28 +22,27 @@ contract KorpusToken_Investment is Ownable {
         require(_to != address(0));
         // Проверяем, что у пользователя достаточно токенов для перевода.
         require(_value <= balances[msg.sender]);
-        // Убираем с баланса оптравителя токены.
+        // Вычитаем с баланса оптравителя токены.
         balances[msg.sender] = balances[msg.sender].sub(_value);
         // Добавляем получателю на баланс токены.
         balances[_to] = balances[_to].add(_value);
         // Ивентируем перевод.
         emit Transfer(msg.sender, _to, _value);
-        // Возращает true, если перевод удался.
         return true;
     }
     
-    // Функция, показывающее баланс пользователя, через адрес.
+    // Функция, показывающее баланс пользователя по адресу.
     function balanceOf(address _owner) public view returns (uint256 balance) {
         // Возращает пользователю баланс.
         return balances[_owner];
     }
     
-    // Маппинг, позволюящий разрешить использовать токены с кошелька другому адресу.
+    // Маппинг с адресами, которые могут использовать токены с определенных кошельков.
     mapping (address => mapping (address => uint256)) internal allowed;
-    // Ивент о разрешении использования токенов.
+    // Ивент разрешения использования токенов.
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    // Функция перевода токенов с одного адреса на другой. Не обязательно, чтобы переводимые токены были на адресе отправителя.
+    // Функция перевода токенов с одного адреса на другой.
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
         // Проверка на пустой адрес.
         require(_to != address(0));
@@ -55,21 +54,20 @@ contract KorpusToken_Investment is Ownable {
         balances[_from] = balances[_from].sub(_value);
         // Токены начисляются на адрес получателя.
         balances[_to] = balances[_to].add(_value);
-        // Забираем право распоряжаться этим количеством токена у адреса оптравителя.
+        // Забираем право распоряжаться этим количеством токенов у адреса оптравителя.
         allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
         // Ивентируем перевод.
         emit Transfer(_from, _to, _value);
-        // Возращает true, если перевод удался.
         return true;
     }
 
-    // Функция, показывающая количество токенов, разрешённых определённым кошельком другому адресу.
+    // Функция, показывающая количество токенов, разрешённых определённым адресом другому адресу.
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-        // Возвращаем количство разрешённых токенов.
+        // Возвращаем количество разрешённых токенов.
         return allowed[_owner][_spender];
     }
     
-    // Функция, разрешающая использовать некоторое количество токенов адреса отправителя другому кошельку.
+    // Функция, разрешающая использовать некоторое количество токенов отправителя какому-либо кошельку.
     function approve(address _spender, uint256 _value) public returns (bool) {
         // Разрешаем использовать токены другому адресу. Если до этого были разрешены какие-то токены, разрешение переназначается.
         allowed[msg.sender][_spender] = _value;
@@ -126,21 +124,20 @@ contract KorpusToken_Investment is Ownable {
         totalSupply = totalSupply.add(_amount);
         // Создаём токены на адресе получателя.
         balances[_to] = balances[_to].add(_amount);
-        // Возвращаем true, если всё прошло успешно.
         return true;
     }
     
-    // Функция сжигания токенов вклада на определённом адресе
+    // Функция сжигания токенов вклада на определённом адресе.
     function burnFrom(address burner, uint256 _value) public {
         // Проверяем, что отправитель может сжечь это количество токенов.
         require(_value <= balances[burner]);
-        // Проверка на то, что отправитель может использовать эти токены в переводе, имеет право их переводить.
+        // Проверка на право отправителя распоряжаться токенами адреса burner.
         require(_value <= allowed[burner][msg.sender]);
-        // Отнимаем от адреса отправителя токены.
+        // Отнимаем от адреса burner токены.
         balances[burner] = balances[burner].sub(_value);
         // Уменьшаем общее количество токенов.
         totalSupply = totalSupply.sub(_value);
-        // Снимаем разрешение на использование сжигаемого количества токенов кошелька пользователя с адреса отправителя.
+        // Снимаем разрешение на использование сжигаемого количества токенов кошелька burner с адреса отправителя.
         allowed[burner][msg.sender] = allowed[burner][msg.sender].sub(_value);
     }
 }
