9da4655a1556e2da31c61dcc6f8f522ae70ee566
PlatonSterh <platon.s14@list.ru>
2019-12-06 20:17:59 +0500

    Add start_read_form DEF.

--- DrawingGraphs.py
+++ DrawingGraphs.py
@@ -1,16 +1,23 @@
 import networkx as nx
-import os
 import pylab as plt
-from google.colab import files 
-def start():
-  uploaded = files.upload()
-  for fn in uploaded.keys():
-    name = fn
-    print('User uploaded file "{name}" with length {length} bytes'.format(name=name, length=len(uploaded[fn])))
-  with open(name, "r", encoding="utf-8-sig") as file:
-    base = file.readlines()
-  for i in range(len(str_list)):
-    base[i] = base[i].rstrip("\n")
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials 
+base = []
+def start_read_form():
+  form = input()
+  worksht = input()
+  scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+  credentials = ServiceAccountCredentials.from_json_keyfile_name("123.json", scope)
+  gc = gspread.authorize(credentials)
+  sh = gc.open(form)
+  worksheet = sh.worksheet(worksht)
+  line = 2
+  while (worksheet.acell('B' + str(line)).value != ""):
+    base.append(worksheet.acell('B' + str(line)).value + " " + worksheet.acell('C' + str(line)).value)
+    base.append(worksheet.acell('B' + str(line)).value + " " + worksheet.acell('D' + str(line)).value)
+    line += 1
+
+def start_draw():
   g = nx.read_edgelist(base)
   nx.draw(nx.DiGraph(g), with_labels=True, node_size=200, edge_Color="black", node_color='yellow', font_color="b", font_size=8, horizontalalignment="center")
   plt.axis('off')
