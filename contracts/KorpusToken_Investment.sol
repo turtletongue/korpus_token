b87453661ac014660f21828f3373305decd900e1
platon <platon.s14@list.ru>
2021-01-15 20:56:47 +0500

    Добавляем роль для ограничения сжигания токенов.

--- contracts/KorpusToken_Investment.sol
+++ contracts/KorpusToken_Investment.sol
@@ -8,15 +8,14 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 // Импортируем стандартные возможности ERC20 токена.
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// Импортируем токен стандарта ERC20, с возможностью сжигания токенов.
-import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
-
-contract KorpusToken_Investment is ERC20, ERC20Burnable, AccessControl {
+contract KorpusToken_Investment is ERC20, AccessControl {
     // Защищаем числа от переполнения.
     using SafeMath for uint256;
 
     // Создаём роль, обладающую правами на создание токенов.
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    // Создаём роль, обладающую правами на сжигание токенов.
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
     // Передаём коструктору ERC20 имя и сокращение токена.
     constructor() ERC20("KorpusToken_Investment", "KTI") {
@@ -29,5 +28,12 @@ contract KorpusToken_Investment is ERC20, ERC20Burnable, AccessControl {
         require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
         // Вызываем внутреннюю функцию создания токенов.
         _mint(to, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public {
+        require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
+        uint256 decreasedAllowance = allowance(account, msg.sender).sub(amount, "ERC20: burn amount exceeds allowance");
+        _approve(account, msg.sender, decreasedAllowance);
+        _burn(account, amount);
     }
 }
